3092ea4f570a68525bb8e40d05bf41934f7eea5f
firdoush ali <mdfirdoushali1776@gmail.com>
2025-02-01 19:55:37 +0530

    About and sticky navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 import Programs from './Components/Programs/Programs.jsx'
 import Title from './Components/Title/Title.jsx'
+import About from './Components/About/About.jsx'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className="container">
       <Title subTitle = 'Our PROGRAM' title='What We Offer'/>
       <Programs/>
+      <About/>
       </div>
       
     </div>
